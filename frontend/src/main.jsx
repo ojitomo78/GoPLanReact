34fab588e06528da386923caff9ca795b3a1f368
Ojitomo <87612008+ojitomo78@users.noreply.github.com>
2026-05-04 09:26:25 +0000

    oooo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// Cambiamos BrowserRouter por HashRouter para mayor compatibilidad en despliegues
+import { HashRouter } from 'react-router-dom'; 
 import Inicio from './pages/Inicio.jsx';
 
 // Importa tus componentes de estructura
-import Header from './components/Header'; // Ajusta la ruta si es necesario
+import Header from './components/Header'; 
 import Footer from './components/Footer'; 
 
-import './css/estilos.css';
+// Importa tus estilos (ya separados si seguiste el consejo anterior)
+import './css/global.css'; 
+import './css/Header.css';
+import './css/Planes.css';
+import './css/Footer.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header /> {/* Tu encabezado aparecerá en todas las páginas */}
+    {/* HashRouter añade un /#/ a la URL, evitando errores 404 en Railway */}
+    <HashRouter> 
+      <Header /> 
       
       <Inicio />
       
-      <Footer /> {/* Tu pie de página aparecerá en todas las páginas */}
-    </BrowserRouter>
+      <Footer /> 
+    </HashRouter>
   </React.StrictMode>
 );
