c68cd5687b50d7e485231f78d79e83ff260802ed
Ojitomo <87612008+ojitomo78@users.noreply.github.com>
2026-05-04 07:46:48 +0000

    sss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Inicio from './pages/Inicio.jsx';
-import './css/estilos.css'; 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Inicio from './pages/Inicio.jsx' // Verifica si es 'Inicio' o 'inicio'
+import './css/estilos.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Inicio />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
