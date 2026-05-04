6a3e71b4d1da216d7f6eff796420a6d7566ca27e
Ojitomo <87612008+ojitomo78@users.noreply.github.com>
2026-05-04 07:35:35 +0000

    siuuu

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Inicio from './inicio.jsx';
+import './css/estilos.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Inicio />
+  </React.StrictMode>
+);
