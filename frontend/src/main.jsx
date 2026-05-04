9bb821210e688c4948aca9a83cb769cee48b89ce
Ojitomo <87612008+ojitomo78@users.noreply.github.com>
2026-05-04 07:43:19 +0000

    sera

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Inicio from './inicio.jsx';
+import Inicio from './pages/inicio.jsx';
 import './css/estilos.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
